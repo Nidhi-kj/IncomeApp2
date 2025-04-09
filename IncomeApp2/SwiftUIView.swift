31061c41c55e96c90e2aa33eadda46da5c5904c7
nancy-jha <nancyjha@example.com>
2025-04-09 21:45:25 +0530

    lecture 1 start

---
+++ IncomeApp2/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  IncomeApp2
+//
+//  Created by Nidhi Jha on 09/04/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello")
+        
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
