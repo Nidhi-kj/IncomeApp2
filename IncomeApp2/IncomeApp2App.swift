0ee77e8f1a527f8b6a94420e8495ec44f8db37d7
nancy-jha <nancyjha@example.com>
2025-04-09 21:22:16 +0530

    Initial Commit

---
+++ IncomeApp2/IncomeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp2App.swift
+//  IncomeApp2
+//
+//  Created by Nidhi Jha on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IncomeApp2Tests/IncomeApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp2Tests.swift
+//  IncomeApp2Tests
+//
+//  Created by Nidhi Jha on 09/04/25.
+//
+
+import Testing
+@testable import IncomeApp2
+
+struct IncomeApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
